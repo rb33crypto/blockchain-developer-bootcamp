a2443d7e1444fb84c28edfdbb7356f03ecc6922c
rb33crypto <rb33crypto@gmail.com>
2024-08-16 15:24:29 -0700

    5. Token Transfers

--- test/Token.js
+++ test/Token.js
@@ -7,7 +7,7 @@ const tokens = (n) => {
 
 describe('Token', ()=> {
 	//contianer for tests
-	let token, accounts, deployer
+	let token, accounts, deployer, receiver
 
 	beforeEach(async ()=> {
 		//1. fetch token from blockchain
@@ -16,6 +16,7 @@ describe('Token', ()=> {
 
 		accounts = await ethers.getSigners()
 		deployer = accounts[0]
+		receiver = accounts[1]
 	})
 
 	describe('Deployment', ()=>{
@@ -48,6 +49,47 @@ describe('Token', ()=> {
 			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 		})	
 	
+	})
+
+	describe('Sending Tokens', ()=> {
+		let amount, transaction, result
+
+		describe('Success', () =>{
+
+			beforeEach(async () =>{
+				amount = tokens(100)
+				transaction = await token.connect(deployer).transfer(receiver.address, amount)
+				result = await transaction.wait()
+			})
+
+			it('transfers token balances', async () => {
+        		expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+        		expect(await token.balanceOf(receiver.address)).to.equal(amount)
+      		})
+			it('emits transfer event', async() => {
+				const event = result.events[0]
+				console.log(event)
+				expect(event.event).to.equal('Transfer')
+
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+				expect(args.to).to.equal(receiver.address)
+				expect(args.value).to.equal(amount)
+
+			})
+		})
+
+		describe('Failure', () => {
+			it('rejects insuffiecient balance', async () => {
+				const invalidAmount = tokens(100000000)
+				await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+			})
+
+			it('rejects invalid recepient', async () => {
+				const amount = tokens(100)
+				await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+			})
+		})
 	})
 
 
