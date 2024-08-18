682080dfaacdde652064a66392693d5ca573602d
rb33crypto <rb33crypto@gmail.com>
2024-08-18 12:43:17 -0700

    final commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ async function main() {
   //fetch contract to deploy
   const Token = await ethers.getContractFactory("Token")
   //deploy contract
-  const token = await Token.deploy()
+  const token = await Token.deploy('DAPP University', 'DAPP', '1000000')
   await token.deployed()
   console.log(`Token Deployed to: ${token.address}`)
 
